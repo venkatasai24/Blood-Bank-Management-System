f25dfad76c195d95a78fa566df261b74d9f46267
VENKATASAI24 <venkatasai24@localhost.localdomain>
2023-11-13 22:48:30 +0530

    merged b-m and m
    merged b-m and m

--- index.php
+++ index.php
@@ -8,7 +8,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/blood-drop.svg" type="image/x-icon">
     <style>
+    html {
+        min-height: 100%;
+        position: relative;
+    }
     /* Add hover effect for links */
     @media (min-width: 576px) {
         .navbar-nav .nav-item a {
@@ -22,6 +28,7 @@
     }
     #animated-image {
     margin-top: 50px;
+    margin-bottom: 100px;
     animation: scaleAnimation 0.4s infinite alternate; /* Animation duration and behavior */
     transform-origin: center; /* Sets the scaling center to the image center */
     }
@@ -38,7 +45,9 @@
 </head>
 <body>
     <!-- Bootstrap navigation bar with responsive button -->
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#FF0000;">
+    <div class="container" style="margin-bottom: 50px;">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color:#FF0000;">
+    <!-- <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#FF0000;"> -->
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -56,8 +65,9 @@
             </ul>
         </div>
     </nav>
+    </div>
 
-    <div class='container text-center' style="color: #ff0000; padding-top: 100px;">
+    <div class='container text-center' style="color: #ff0000; padding-top: 100px;padding-bottom:50px;">
         <h1 class="display-6">Blood Bank Management System</h1>
         <p class="lead mt-3">
             This system is designed to efficiently manage blood donations, donors, and recipients, ensuring the availability of safe and life-saving blood for those in need.
@@ -65,7 +75,7 @@
         <p class="lead mt-3">
             Join us in the mission to save lives. Register as a donor or recipient and help make a difference!
         </p>
-        <img id="animated-image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjKpuNmdiLq3rilYv0ClyQlEIcVPwg1Z8Xz8kQ8sHZ&s" alt="">
+        <img id="animated-image" src="images/blood.png" alt="">
     </div>
 
 
@@ -75,4 +85,8 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+<footer class="footer" style="background-color: #FF0000; color: #FFF; padding: 15px; text-align: center; position: absolute; bottom: 0; width: 100%;">
+    <!-- Add content for your footer here -->
+    &copy; <a style="color:#FFF;" href="https://github.com/venkatasai24">Venkatasai24</a> and Team. All rights reserved. 2023
+</footer>
 </html>
