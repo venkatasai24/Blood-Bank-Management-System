cf4167703d89eb82926a064d7b599342373b9e3f
VENKATASAI24 <venkatasai24@localhost.localdomain>
2023-11-14 00:35:18 +0530

    css updated

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,9 +4,6 @@
         $dbname= "XXXXXXXXXXXX";
         $dbusername= "XXXXXXXXXXX";
         $dbpassword= 'XXXXXXXXXXX';
-        $dbname = 'BBMS';
-        $dbusername = "root";
-        $dbpassword = "";
 
         try {
             $pdo = new PDO("mysql:host=$host;dbname=$dbname", $dbusername, $dbpassword);
